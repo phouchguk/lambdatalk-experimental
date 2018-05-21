b19825092b949ce0fbff0a42f0ce6907a273553e
Graham Simkins <graham.simkins@concerto.co.uk>
2018-05-21 20:25:38 +0100

    what order does eval happen

--- lte.js
+++ lte.js
@@ -1,15 +1,30 @@
+/* globals console */
+
 (function() {
   "use strict";
 
   var evl, evlForm, re, test;
 
-  test = "{{fn {fruit unknown} The colour of fruit is unknown.} apple green}";
+  test =
+    "{{fn {fruit unknown} The colour of {if true then fruit else veg} is unknown.} apple green}";
+
   re = /\{([^\s{}]*)(?:[\s]*)([^{}]*)\}/g;
+
+  evlForm = function() {
+    var f, r;
+
+    f = arguments[1] || "";
+    r = arguments[2] || "";
+
+    console.log(" - " + f + " : " + r);
+
+    return "_evl_";
+  };
 
   evl = function(s) {
     do {
       console.log(s);
-    } while (s !== (s = s.replace(re, "_evl_")));
+    } while (s !== (s = s.replace(re, evlForm)));
 
     return s;
   };
